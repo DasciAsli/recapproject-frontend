5e1df1932d2b7aa4738be9fc64f88d2864c54aff
AsliDasci <aslihandasci1@gmail.com>
2022-09-26 20:45:58 +0300

    Add other components and services
    Brand,color,customer,rental

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    RentalComponent,
   ],
   imports: [
     BrowserModule,
