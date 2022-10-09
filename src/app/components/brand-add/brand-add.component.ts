785ccbd8a5e1975abbc6532430f67901f656b49d
AsliDasci <aslihandasci1@gmail.com>
2022-10-09 15:17:08 +0300

    Complete the brandAddComponent

--- src/app/components/brand-add/brand-add.component.ts
+++ src/app/components/brand-add/brand-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,FormControl ,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand-add',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brand-add.component.css']
 })
 export class BrandAddComponent implements OnInit {
+  brandAddForm:FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private brandService:BrandService,
+    private toastrService:ToastrService,
+    private router:Router
+    ) { }
 
   ngOnInit(): void {
+    this.createBrandAddForm();
+  }
+
+  createBrandAddForm(){
+    this.brandAddForm= this.formBuilder.group({
+      brandName:["",Validators.required]
+    })
+  }
+
+  brandAdd()
+  {
+    if(this.brandAddForm.valid)
+    {
+      let brandModel=Object.assign({},this.brandAddForm.value);
+      console.log(brandModel);
+      this.brandService.addBrand(brandModel).subscribe(response=>
+        {
+          this.toastrService.info(response.message,"Marka eklendi");
+          this.router.navigate(["/brands"]);
+        })
+    }
+    else
+    {
+      this.toastrService.error("Lütfen formu doğru bir şekilde doldurunuz");
+    }
   }
 
 }
--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -3,14 +3,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/listResponseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
-  apiUrl="https://localhost:44362/api/brands/getall"
+  apiUrl="https://localhost:44362/api/brands"
+
   constructor(private httpClient:HttpClient) { }
+
   getBrands():Observable<ListResponseModel<Brand>>{
-    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+    let newPath=this.apiUrl + "/getall";
+    return this.httpClient.get<ListResponseModel<Brand>>(newPath);
+  }
+
+  addBrand(brand:Brand):Observable<SingleResponseModel<Brand>>{
+    let newPath=this.apiUrl +"/add";
+    return this.httpClient.post<SingleResponseModel<Brand>>(newPath,brand);
   }
 }
